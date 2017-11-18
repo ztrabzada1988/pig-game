bb50d37a9a858f6778df2ed137c09117517fb90e
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-11-17 18:14:41 -0800

    adding javascript to manipulate DOM

--- app.js
+++ app.js
@@ -9,13 +9,62 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+// in the beggening of the game we dont want to see the dice
+document.querySelector('.dice').style.display = 'none';
 
-console.log(dice);
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// Anonymous function is one that doesnt have name so cannot be used
+// Callback function is one that is passed in another function as an argument
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number 
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Display the result 
+    var diceDOM = document.querySelector('.dice');    
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // if 1 is rolled Next player should be the active player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.plater-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // Add CURRENT score to GLOBAL score 
+    
+
+    // Update the UI 
+
+    // Check if player won the game 
+})
+
+
+
+
 
